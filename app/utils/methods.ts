672bb0392b63b7371a05c44d06812c3dde5da881
youssef-md <emaildeyoussefmuhamad@gmail.com>
2021-02-10 03:14:31 -0300

    Update formatBRL and create formatDate util method

--- app/utils/methods.ts
+++ app/utils/methods.ts
@@ -1,14 +1,29 @@
 import 'intl';
-import 'intl/locale-data/jsonp/en';
+import 'intl/locale-data/jsonp/pt';
 
 export function formatBRL(number: string): string {
   const formatted = Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
-  })
-    .format(Number(number))
-    .replace(',', '.')
-    .replace(/.([^.]*)$/, ',' + '$1');
+  }).format(Number(number));
+
+  return formatted;
+}
+
+export function formatDate(date: string): string {
+  const formattedDate = Intl.DateTimeFormat('pt-BR', {
+    day: 'numeric',
+    month: 'numeric',
+    year: 'numeric',
+  }).format(new Date(date));
+
+  const formattedHour = Intl.DateTimeFormat('pt-BR', {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  }).format(new Date(date));
+
+  const formatted = `${formattedDate} às ${formattedHour}`;
 
   return formatted;
 }
